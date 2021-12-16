1135b4efa31a3e55884b6ab7bdf952f06bf46f36
torikatsu <torikatsu923@gmail.com>
2021-12-16 22:37:01 +0900

    add outline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![allow(unused_variables)] // ignore unused variables
+
+type File = String;
+
+fn open(f: &mut File) -> bool {
+    true // always success
+}
+
+fn close(f: &mut File) -> bool {
+    true // always success
+}
+
+#[allow(dead_code)] // ignore unused function
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
+    unimplemented!() // crash program
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut f1 = File::from("f1.txt");
+    open(&mut f1);
+    // read(f1, vec1[]);
+    close(&mut f1);
 }
