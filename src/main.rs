b115c8954928a45cc3335f63652cd6822480d85d
torikatsu <torikatsu923@gmail.com>
2021-12-16 23:17:03 +0900

    implement read function

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
-#![allow(unused_variables)] // ignore unused variables
+#![allow(unused_variables)]
 
-type File = String;
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
 
 fn open(f: &mut File) -> bool {
-    true // always success
+    true
 }
 
 fn close(f: &mut File) -> bool {
-    true // always success
+    true
 }
 
-#[allow(dead_code)] // ignore unused function
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!() // crash program
+fn read(f: &File, save_to: &mut Vec<u8>, ) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
 }
 
 fn main() {
-    let mut f1 = File::from("f1.txt");
-    open(&mut f1);
-    // read(f1, vec1[]);
-    close(&mut f1);
+    let mut f2 = File {
+        name: String::from("f1.txt"),
+        data: vec![114, 117, 115, 116, 33],
+    };
+
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f2);
+    let f2_length = read(&f2, &mut buffer);
+    close(&mut f2);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{} is {} bytes long", &f2.name, f2_length);
+    println!("{}", text);
 }
