ebf578f95f78d055d887b3751db38f757f9ab8cc
torikatsu <torikatsu923@gmail.com>
2021-12-17 21:06:43 +0900

    document comments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+//! Simulate File
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
+use std::fmt;
+use std::fmt::{Display};
+
 #[derive(Debug, PartialEq)]
-enum FileState {
+pub enum FileState {
     Open,
     Closed,
 }
 
+/// file means accesible file
 #[derive(Debug)]
-struct File {
+pub struct File {
     name: String,
     data: Vec<u8>,
     state: FileState,
@@ -16,6 +21,15 @@ struct File {
 
 trait Read {
     fn read(self: &Self, save_to: &mut Vec<u8>) -> Result<usize, String>;
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
 }
 
 impl Read for File {
@@ -34,8 +48,21 @@ impl Read for File {
     }
 }
 
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter,) -> fmt::Result {
+        write!(f, "<{} {}>", self.name, self.state)
+    }
+}
+
 impl File {
-    fn new(name: &str) -> File {
+    /// new file consider as empty, but file name is required.
+    /// 
+    /// # examples
+    /// ```
+    /// let f = File::new("f1.txt");
+    /// ```
+    /// 
+    pub fn new(name: &str) -> File {
         File {
             name: String::from(name),
             data: Vec::new(),
@@ -43,10 +70,20 @@ impl File {
         }
     }
 
-    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+    pub fn new_with_data(name: &str, data: &Vec<u8>) -> File {
         let mut f = File::new(name);
         f.data = data.clone();
         f
+    }
+
+    /// return file size(byte).
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    /// return file name.
+    pub fn name(&self) -> String {
+        self.name.clone()
     }
 }
 
@@ -61,21 +98,8 @@ fn close(mut f: File) -> Result<File, String> {
 }
 
 fn main() {
-    let mut f5 = File::new("5.txt");
+    let f5 = File::new("5.txt");
 
-    let mut buffer: Vec<u8> = vec![];
-
-    if f5.read(&mut buffer).is_err() {
-        println!("Error checking is working");
-    }
-
-    f5 = open(f5).unwrap();
-    let f5_length = f5.read(&mut buffer).unwrap();
-    f5 = close(f5).unwrap();
-
-    let text = String::from_utf8_lossy(&buffer);
-
-    println!("{:?}", 15);
-    println!("{} is {} bytes long", &f5.name, f5_length);
-    println!("{}", text);
+    println!("{:?}", f5);
+    println!("{}", f5);
 }
